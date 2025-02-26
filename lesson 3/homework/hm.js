21dc027fefa19362e2c6f01dc23e31d21f83172a
lukamerumovi <lukalukamerumeru@gmail.com>
2025-02-26 13:07:29 +0400

    commit

---
+++ lesson 3/homework/hm.js
@@ -0,0 +1,14 @@
+function changeColorAndText() {
+    let b1 = document.getElementById("b1")
+    b1.style.color="blue"
+    b1.style.backgroundColor="green"
+    b1.textContent="IT"
+
+}
+
+function changeColor(){
+    let b2 = document.getElementById("b2")
+    b2.style.color="yellow"
+    b2.style.backgroundColor="pink"
+    b2.textContent="Soho"
+}
---
+++ lesson 4/classwork/cs.js
@@ -0,0 +1,18 @@
+// alt shift f
+// let nickName = "luka";
+// let _nickName = "giorgi";
+// let _nick50Name = "levani";
+// let nick$Name = "nika";
+// let nick29481 = "andria";
+
+
+
+function displayInformation() {
+    let name = document.getElementById("name").value
+    let email = document.getElementById("email").value
+    let age = document.getElementById("age").value
+    let p = document.getElementById("p")
+    p.textContent = "Hello" + name + email + age
+
+
+}   
