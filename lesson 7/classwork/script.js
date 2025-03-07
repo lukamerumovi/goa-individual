e1de46b744894b6c1dc9b911531249449b746ac6
lukamerumovi <lukalukamerumeru@gmail.com>
2025-03-07 12:53:16 +0400

    commit

---
+++ lesson 7/classwork/script.js
@@ -0,0 +1,15 @@
+// let form = document.getElementById("form")
+
+// // let createdName = ""
+// // let createdAge = 0
+// // let createdEmail = ""
+// // let createdPass = ""
+
+// function createAccount(){
+//     let username = form.elememts.username.value
+//     let userage = form.elements.userage.value
+//     let useremail = form.elements.useremail.value
+//     let userpass = form.elements.userpass.value
+
+//     console.log(username, userage,useremail, userpass)
+// }
---
+++ lesson 7/classwork/script1.js
@@ -0,0 +1,15 @@
+let age =20
+
+// if(age <12){
+//     console.log("u are child")
+// }else if (13<age<19){
+//     console.log("u are a teenager")
+// }else if (age > 20){
+//     console.log("u are an adult")
+// }
+
+// if(age% 2 ==0){
+//     console.log("even")
+// }else if (age % 2 != 0){
+//     console.log("odd")
+// }
