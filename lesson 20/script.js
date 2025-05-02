cffdc1a59037482ead661c69e6614607f095a99b
lukamerumovi <lukalukamerumeru@gmail.com>
2025-05-02 13:50:51 +0400

    commit

---
+++ lesson 20/script.js
@@ -0,0 +1,111 @@
+// ყველა ელემენტები წამოვიღოთ აიდის საშუალებით
+// vqmnit saxelis shemyvan funqciashi
+// gilakis daweris dros gamogvitanos random ricxvi sheadaros ertmanets da romelic ufro meti iqneba mas mieces qula
+// reset daweris shemtxvevashi yvela monacemi ganuldes
+
+// let userName = document.getElementById("name-p")
+// let mainPoint1 = document.getElementById("main-point1")
+// let mainPoint2 = document.getElementById("main-point2")
+// let randomPoint1 = document.getElementById("random1")
+// let randomPoint2 = document.getElementById("random2")
+// let rollBtn = document.getElementById("roll-btn")
+// let resetBtn = document.getElementById("reset-btn")
+// let namePs = document.getElementById("name-p")
+
+// let name = prompt("Enter ur name")
+// while(name == ""){
+//     alert("please enter youre name")
+//     prompt("enter name")
+// }
+// namePs.innerHTML = name
+// let score1 = 0
+// let score2 = 0 
+// rollBtn.addEventListener("click", function(){
+//     let num1 = Math.floor( Math.random() *6 ) +1
+//     let num2 = Math.floor(Math.random() *6) +1
+//     randomPoint1.innerHTML = num1
+//     randomPoint2.innerHTML = num2
+//     if(num1 > num2){
+//         score1 +=1
+//         mainPoint1.innerHTML = score1
+//     }else if(num2 > num1){
+//         score2 +=1
+//         mainPoint2.innerHTML = score2
+//     }else if (num1 === num2){
+//         alert("its a draw")
+//     }
+// }) 
+// resetBtn.addEventListener("click", function(){
+//     randomPoint1.innerHTML = 0
+//     randomPoint2.innerHTML = 0
+
+//     mainPoint1.innerHTML = 0
+//     mainPoint2.innerHTML = 0
+// })
+
+
+
+// for(let i = 0; i<= 10; i++){
+//     console.log(i)
+// }
+
+
+// for(let i = 2;i<=20; i+=2 ){
+//     console.log(i)
+// }
+
+
+
+
+// let i = 1
+// while(i<5){
+//     console.log(i)
+//     i += 1
+// }
+
+
+// let sum = 0
+// let num = 1
+
+// while(num <=100){
+//     sum += num
+//     num+=1
+// }console.log(sum)
+
+
+// let num = 1
+// if(num == 0){
+//     alert("the num is 0")
+// }else if(num > 0){
+//     alert("positive")
+// }else if(num <0){
+//     alert("negative")
+// }
+
+
+// let num = 2
+// if(num %2 ==0){
+//     console.log("even")
+// }else if (num %2 !=0){
+//     console.log("odd")
+// }
+
+// age = 17
+// if (age <18){
+//     alert("xmis micema ar shegidzlia")
+// }else if (age >18){
+//     alert("xmis micema shegidzlia")
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
