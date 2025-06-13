d3a6b7d20a3e944ab15e362bbea812b7d4fba6ab
lukamerumovi <lukalukamerumeru@gmail.com>
2025-06-13 18:08:33 +0400

    commit

---
+++ lesson 31/classwork/script.js
@@ -0,0 +1,34 @@
+// 1 change imagoe source have an mg element with an IdleDeadline. 
+// onbutton clck change the image to a different src
+
+// document.getElementById("theButton")
+// document.getElementById("image").src = "https://images.search.yahoo.com/search/images;_ylt=AwrNOQ05HkxoSkEA611XNyoA;_ylu=Y29sbwNiZjEEcG9zAzIEdnRpZAMEc2VjA3Ny?type=E210US91215G0&p=Bing&fr=mcafee&imgurl=https%3A%2F%2Fwww.bing.com%2Fimages%2Fsearch%3Fview%3DdetailV2%26ccid%3DKW4Vccpl%26id%3DE5FCE02DE1705CF178C997B1F98898340DB5536E%26thid%3DOIP.KW4Vccpl7Npois5wDoM8HgAAAA%26mediaurl%3Dhttps%3A%2F%2Fth.bing.com%2Fth%2Fid%2FOIP.6ys8C0MpJ98pMq97FqvdfQHaJQ%3Fw%3D236%26h%3D295%26c%3D7%26o%3D5%26pid%3D1.20%26q%3Dimage%26ck%3DADEC9284CA4C0ABDD1C8DE81DF2BE05A%26idpp%3Drc%26idpview%3Dsingleimage%26form%3Drc2idp&name=image4&turl=https%3A%2F%2Fsp.yimg.com%2Fib%2Fth%3Fid%3DOIP.KW4Vccpl7Npois5wDoM8HgAAAA%26pid%3DApi%26w%3D148%26h%3D148%26c%3D7%26dpr%3D2%26rs%3D1&tt=image4&sigit=mU6PmcZq0HJl&sigi=x0jnKeD8B7IX&sign=MVMvkCZPQY5Y&sigt=MVMvkCZPQY5Y"
+
+// 2 have a paragraph and button.
+// on click change the paragraphs text color to red
+
+// let butt = document.getElementById("theBtn")
+// butt.addEventListener('click', function(){
+// document.getElementById("theP").style.color = "red"
+// })
+
+// make a button that hides and shows a paragraph (display:none/block)
+// let paragraph = document.getElementById("theParagraph")
+// let button = document.getElementById("theBut")
+// let hide = false
+// button.addEventListener('click',function(){
+//     if(!hide){
+//         paragraph.style.display = "none"
+//         hide = true
+//     }else if (hide){
+//         paragraph.style.display = "block"
+//         hide = false
+//     }
+// })
+
+// // create an imput of type password 
+
+// let inp = document.getElementById("theInput")
+// inp.addEventListener('click', function(){
+    
+// })
