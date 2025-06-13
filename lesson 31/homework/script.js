017d4218edd4dcd4c53cf3e56c12bd53e0b78ccb
lukamerumovi <lukalukamerumeru@gmail.com>
2025-06-13 16:05:26 +0400

    commit

---
+++ lesson 31/homework/script.js
@@ -0,0 +1,64 @@
+// create a paragraph with an ID
+// use js to cahnge its the text content afther the page loads
+
+// let mic = document.getElementById("me")
+// mic.textContent="voice"
+
+// Add a button
+// When its clicked, change the background color of the body.
+
+// let button = document.getElementById("btn")
+// document.body.style.backgroundColor = "green"
+
+// create a paragraph and a button
+// when the button is clicked hide/show the paragraph !!!???
+// let paragraph = document.getElementById("post")
+// let butt = document.getElementById("buTon")
+
+// let hide = false
+
+// butt.addEventListener('click',function(){
+//   if(!hide){
+//     paragraph.style.display = "none"
+//     hide = true
+//   }else if(hide){
+//     paragraph.style.display = "block"
+//     hide = false
+//   }
+// })
+
+// Add a text input and an empty <h2>
+// when the user types something and clicks a button
+// display the input text in the heading!!!???
+
+// let input = document.getElementById("theInput")
+// let btn = document.getElementById("theButton")
+// let h2 = document.getElementById("theH")
+
+// btn.addEventListener('click',function(){
+//   h2.textContent = input.value
+// })
+
+// create 3 paragraph with the same class
+// when button is clicked change the text color of all those paragraphs
+
+// let btn = document.getElementById("theB")
+// let paragraphs = document.getElementsByClassName("theP")
+
+// btn.addEventListener('click',function(){
+//   for(let i = 0;i < paragraphs.length;i++){
+//     paragraphs[i].style.color = "red"
+//   }
+// })
+
+// add a text input and a button 
+// when the button is clicked add the input value as a new list item to a <ul>.
+// let btn = document.getElementById("aButton")
+// let inp = document.getElementById("theInput")
+// let ul = document.getElementById("theUL")
+
+// btn.addEventListener('click',function(){
+//   let li = document.createElement("li")
+//   li.textContent = inp.value
+//   ul.appendChild(li)
+// })
