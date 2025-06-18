7ed89c70cb2110afa1f46b2f67112d65937b7700
lukamerumovi <lukalukamerumeru@gmail.com>
2025-06-18 15:04:20 +0400

    commit

---
+++ lesson 32/homework/hm.py
@@ -0,0 +1,24 @@
+# def triple(arr):
+#     newList = []
+#     for i in arr:
+#         newList.append(i * 3)
+#     return newList
+# print(triple([1,2,3])) 
+
+
+
+
+# def square(arr):
+#     newList = []
+#     for i in arr:
+#         newList.append(i ** 2)
+#     return newList
+# print(square([1,2,3])) 
+
+
+# def negative(arr):
+#     newList = []
+#     for i in arr:
+#         newList.append(i * -1)
+#     return newList
+# print(negative([1,2,3])) 
---
+++ lesson 32/homework/script.js
@@ -0,0 +1,33 @@
+// function theNumbers(num){
+// let sum = 0
+// for(let i = 0; i < num.length; i++ ){
+//     if (num > 0){
+//         sum += 1
+//     }
+// }
+// return sum
+// }
+
+// function aNumber(nums){
+//     let sum = 0
+//     for (let i = 0; i<nums.length; i++){
+//         if(nums %2 == 0){
+//             sum += nums
+//         }
+//     }
+//     return sum
+// }
+
+// write a function that counts how many vowels are in a given string.
+
+// function theVowels(string){
+//     let st = "aeiouAEIOU"
+//     let sum = 0
+//     for(let i = 0; i < string.length; i++){
+//         if (st.includes(string[i])){
+//             sum += 1
+//         }
+//     }
+//     console.log(sum);
+// }
+// theVowels("strai")
