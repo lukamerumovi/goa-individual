c22ce64956aa1c8e0942c41e06855f26dff75fbe
lukamerumovi <lukalukamerumeru@gmail.com>
2025-06-24 22:57:42 +0400

    commit

---
+++ lesson 34/classwork/script.js
@@ -0,0 +1,50 @@
+
+// Create a counter that logs numbers from 1 to 5 to the console every second, then stops.
+
+// let count = 1
+// let setInterval = setInterval(function(){
+//     console.log(count)
+//     if (count === 5){
+//         clearInterval(setInterval)
+//         count+=1
+//     }
+// },1000)
+
+
+
+
+
+// Make a message display in the console every 2 seconds, and stop it after 10 seconds.
+
+// let timer = setInterval(function(){
+//     console.log('display every 2 seconds')
+// let timeout = clearInterval(timer)
+//     console.log("stops after 10 seconds")
+// },2000)
+
+
+// write a programm that shows "hello world" 3 seconds after clicking a button."
+// make the button say "clicked" while waiting.
+
+// let button = document.getElementById("theBTN")
+// button.addEventListener('click', function(){
+//     let timeout = setTimeout(function(){
+//         console.log("Hello World")
+//     },3000)
+// })
+
+
+// create a countdown from 5 to 0 using setTimeout, showing one number per second in the browser.
+
+
+// let countDown = 5
+// for(let i = 5; i >= 0; i--){
+// }
+
+
+
+
+
+
+
+
---
+++ lesson 34/homework/script.js
@@ -0,0 +1,72 @@
+// Create a counter that logs numbers from 1 to 5 to the console every second, then stops.
+
+// let count = 1
+// let setInterval = setInterval(function(){
+//     console.log(count)
+//     if (count === 5){
+//         clearInterval(setInterval)
+//         count+=1
+//     }
+// },1000)
+
+
+
+
+
+// Make a message display in the console every 2 seconds, and stop it after 10 seconds.
+
+// let timer = setInterval(function(){
+//     console.log('display every 2 seconds')
+// let timeout = clearInterval(timer)
+//     console.log("stops after 10 seconds")
+// },2000)
+
+
+// write a programm that shows "hello world" 3 seconds after clicking a button."
+// make the button say "clicked" while waiting.
+
+// let button = document.getElementById("theBTN")
+// button.addEventListener('click', function(){
+//     let timeout = setTimeout(function(){
+//         console.log("Hello World")
+//     },3000)
+// })
+
+
+// create a countdown from 5 to 0 using setTimeout, showing one number per second in the browser.
+
+
+// let countDown = 5
+// for(let i = 5; i >= 0; i--){
+// }
+
+
+// make a div that slowly change colors.
+
+// let div = document.getElementById("theDiv")
+// box.style.backgrounColor = "purple"
+// setTimeout(function(){
+//     box.style.backgrounColor = "yellow"
+// },2000)
+// setTimeout(function(){
+//     box.style.backgrounColor = "green"
+// },2000)
+
+
+
+// create a typing effect with "setTimeout"???
+
+// let text = "testing type of the text"
+// let div = document.getElementById("textDiv")
+
+
+
+// show 3 alert messages each one second apart
+
+alert("1 alert message")
+setTimeout((){
+    alert("2 alert message")
+},1500)
+setTimeout((){
+    alert("3 alert message")
+},1500)
