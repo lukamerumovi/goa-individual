47af784bfbf849ece245b05a40dc729a00732d43
lukamerumovi <lukalukamerumeru@gmail.com>
2025-06-27 13:54:10 +0400

    commit

---
+++ lesson 36/classwork/scqipt.js
@@ -0,0 +1,89 @@
+// def string_to_number(s):
+//     return int(s)
+
+
+// def count_sheeps(sheep):
+//     sum = 0
+//     for i in sheep:
+//         if i == True:
+//             sum += 1
+//     return sum
+
+
+// function booleanToString(b){
+//   return str(b)
+// }
+
+
+// def basic_op(operator, value1, value2):
+//     if operator == "+":
+//         return value1 + value2
+//     elif  operator == "-":
+//         return value1 - value2
+//     elif operator == "*":
+//         return value1 * value2
+//     elif operator == "/":
+//         return value1 / value2
+
+// def double_integer(i):
+//     return i * 2
+
+
+
+// def greet(name):
+//     return "Hello, " + name + " how are you doing today?"
+
+
+// def no_space(x):    
+//     return x.replace(" ", "")
+
+// function updateLight(current) {
+//   if(current == "green"){
+//     return "yellow";
+//   }else if (current == "yellow"){
+//     return "red"
+//   }else if(current == "red"){
+//     return "green"
+//   }
+// }
+
+
+
+
+// function setAlarm(employed, vacation){
+//   return employed && !vacation
+// }
+
+
+
+// function greet(name){
+//   if(name === "Johnny"){
+//     return "Hello, my love!";
+//   }else{
+//       return "Hello, " + name + "!";
+//   }
+// }
+
+
+
+
+// function sumMix(x){
+//   let newArr = []
+//   let sum = 0
+//   for (let i =0; i< x.length; i++){
+//     newArr.push(Number(x[i]))
+//   }
+//   for(let x = 0;x<newArr.length;x++){
+//     sum+=newArr[x]
+//   }
+//   return sum
+// }
+
+
+
+
+
+
+
+
+
