324c59a08fe998363fb284f3a4592dda4cf79803
lukamerumovi <lukalukamerumeru@gmail.com>
2025-09-03 21:02:55 +0400

    commit

---
+++ lesson 37/classwork/script.js
@@ -0,0 +1,14 @@
+// def to_jaden_case(string):
+    
+//     arr2 = []
+
+//     arr = string.split()
+//     for i in arr:
+//        arr2.append(i[0].upper() + i[1:].lower())
+
+//     stringed_arr = " ".join(arr2)
+
+//     return stringed_arr
+
+
+
---
+++ lesson 38/classwork/cw.py
@@ -0,0 +1,31 @@
+# int- mteli ricxvi magalitad 3 
+# float - atwiladi ricxvi mag 3,5
+# boolean - true or false 
+# str - text
+
+# name = "luka"
+# print (name)
+
+
+
+# for i in range(10):
+#     print(i)
+
+
+# age = int(input("sheikvanet tqv4eni asaki"))
+# if age > 18:
+#     print("u are adult")
+# elif age <18 and age >14:
+#     print("u are not enought")
+# else:
+#     print("u are kid")
+
+
+# def the_guy():
+#     name = "გიორგი"
+#     age = "22"
+#     return name, age
+# print(the_guy())
+
+
+
