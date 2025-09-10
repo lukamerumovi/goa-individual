9bc1a34c7a03fdd7c9b16d52faac9deb0e5d394e
lukamerumovi <lukalukamerumeru@gmail.com>
2025-09-10 21:52:18 +0400

    commit

---
+++ lesson 40/classwork/cw.py
@@ -0,0 +1,32 @@
+#codewars reversed words
+
+# def reverse_words(s):
+#     return " ".join(s.split()[::-1])
+
+
+# def find_average(numbers):
+#     if numbers == []:
+#         return 0
+#     return sum(numbers) / len(numbers)
+
+
+# classwork
+# def remove_char(s):
+#     return s[1:len(s)-1]
+
+
+# def monkey_count(n):
+#     list = []
+#     for i in range(1,n+1):
+#         list.append(i)
+#     return list
+
+
+# def points(games):
+#     score = 0
+#     for game in games:
+#         if game[0] > game[2]:
+#             score += 3
+#         elif game[0] == game[2]:
+#             score +=1
+#     return score
