1299870c75fafefb451e968a3ff07feebd4f61e1
lukamerumovi <lukalukamerumeru@gmail.com>
2025-09-19 22:04:48 +0400

    commit

---
+++ lesson 42/classwork/script.js
@@ -0,0 +1,47 @@
+// შექმენი ფუნქცია რომელსაც არგუმენტად გადაეცემა სტრინგი, for-ით უნდა გადაუყვე ამ სტრინგს და მოაშორო ყოველი კენტი ინდექსის მქონე ასო
+
+//     let str1 = "some of the oldest"
+//     let arr1 = [12,42,"go", true, "yes", 1]
+
+
+//     function func1(str){
+//         let st2 = ""
+//     for(let i =0; i < str.length; i++){
+//         if (i % 2 != 0){
+//             st2+=str[i]
+//         }
+//     }
+//     return st2
+// }
+//     console.log(func1(str1))
+
+
+
+// შექმენი ფუნცია რომელსაც არგუმენტად გადაეცემა მასივი, ამ ფუნქციამ ყოველი მესამე ელემენტი უნდა მოაშოროს მასივიდან
+
+//    let arr1 = [12,42,"go", true, "yes", 1]
+
+// function func2(arr){
+//     let newArr = []
+//     for (let i =0; i < arr.length; i++){
+//         if (i %3 !=0){
+//             newArr.push(arr[i])
+//         }
+//     }
+//     return newArr
+// }
+
+// შექმენი ფუნქცია რომელიც ყოველ ლუწ ინდექსზე ამატებს ინდექსის ნომერს.
+
+//    let arr1 = [12,42,"go", true, "yes", 1]
+// function func3(arr){
+//     let newArr = []
+//     for (let i =0;i<arr.length;i++){
+//         if ( i %2 == 0){
+//             newArr.push(i)
+//         }else {
+//             newArr.push(arr[i])
+//         }
+//     }
+//     return newArr
+// }
