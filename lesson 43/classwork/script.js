5d89ee95be7fa14dcbe22a36b0db8ecd77760100
lukamerumovi <lukalukamerumeru@gmail.com>
2025-09-21 19:54:11 +0400

    commit

---
+++ lesson 43/classwork/script.js
@@ -0,0 +1,14 @@
+// შექმენი ფუნქცია რომელსაც გადაეცემა ორი არგუმენტი, პირველი არგუმენტი არის რაიმე ასო მაგალითად "a" 
+// და მეორე არგუმენტი არის სტრინგი მაგალითად "peaceful night" შენი დავალებაა გადმოცემული პირველი 
+// არგუმენტი ("a") მოაშორო ამ სტრინგიდან
+
+// let arg1 = "a"
+// let arg2= "peaceful night"
+// function func(arg,argument){
+//     let equal = ""
+//     for(let i = 0; i <argument.length; i++){
+//         if(argument[i] != arg){
+//             equal = equal + argument[i]
+//         }
+//     }return equal
+// }
